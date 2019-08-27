3e09a6cf5922a2b9f8f9a323b4a08f40a620ed3e
alexxxstep <astep2004@gmail.com>
2019-08-27 09:07:36 +0300

    update all

--- tasks/blueprint.py
+++ tasks/blueprint.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask import render_template
-from models import Task
+from models import Task, Tag
 
 tasks = Blueprint('tasks', __name__, template_folder='templates')
 
@@ -13,4 +13,13 @@ def index():
 @tasks.route('/<slug>')
 def task_detail(slug):
     task = Task.query.filter(Task.slug==slug).first()
-    return render_template('tasks/task_detail.html', task=task)
+    tags = task.tags
+    return render_template('tasks/task_detail.html', task=task, tags=tags)
+
+#http://localhost/task/tag/python
+@tasks.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug==slug).first()
+    tasks = tag.tasks.all()
+    return render_template('tasks/tag_detail.html', tag=tag, tasks=tasks)
+
