7b25fd231d29d8909d9923b7f83861359bec27d3
alexxxstep <astep2004@gmail.com>
2019-09-13 23:10:25 +0300

    current update

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from flask import redirect, url_for, request
+
 from app_models.config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 from flask_admin import Admin
+from flask_admin import AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 
 from flask_security import SQLAlchemyUserDatastore
 from flask_security import Security
+from flask_security import current_user
+
+
 
 app = Flask(__name__)
 
@@ -27,9 +33,26 @@ manager.add_command('db', MigrateCommand)
 
 # ADMIN
 from models import *
-admin = Admin(app)
-admin.add_view(ModelView(Task, db.session))
-admin.add_view(ModelView(Tag, db.session))
+
+class AdminView(ModelView):
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('security.login', next=request.url))
+
+class HomeAdminView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.has_role('admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('security.login', next=request.url))
+
+
+
+admin = Admin(app, 'FlaskApp', url='/', index_view=HomeAdminView('Home'))
+admin.add_view(AdminView(Task, db.session))
+admin.add_view(AdminView(Tag, db.session))
 
 # FLASK security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
